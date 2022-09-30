98bb7f1519704d911adf7d0a0b7ef01ad2a128f7
deepak-contentstack <deepak.bulani@contentstack.com>
2022-09-30 17:28:42 +0530

    feat: updated coverage report

--- test/jest.config.js
+++ test/jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
     '<rootDir>/src/components/RichTextEditor/SuperChargedRte/elements/Element/align/alignCenter/*.{js,jsx,ts,tsx}',
     '!**/vendor/**'
   ],
-  coverageReporters: ['lcov'],
+  coverageReporters: ['lcov', 'json'],
   coverageThreshold: {
     global: {
       branches: 0,
